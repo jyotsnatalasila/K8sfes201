72da01c1aa822cd6111d611b52664fbf373981e2
jyotsnatalasila <jyotsnatalasila@gmail.com>
2025-11-03 14:51:39 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://16.16.28.69:30083/auth";
+const API_URL = "http://ec2-16-16-80-131.eu-north-1.compute.amazonaws.com:30083/auth";
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
